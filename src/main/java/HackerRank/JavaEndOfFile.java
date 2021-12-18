f51b27f40b9cc58b0bee5c50eeca2eebc865f7c3
alvindivina08 <alvindivina08@gmail.com>
2021-12-18 02:22:06 -0600

    java end of file solution

---
+++ src/main/java/HackerRank/JavaEndOfFile.java
@@ -0,0 +1,16 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class JavaEndOfFile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 1;
+        String a;
+        while (scanner.hasNext()){
+            a = scanner.nextLine();
+            System.out.println(n + " " + a);
+            n++;
+        }
+    }
+}
