16ec3417c9ee7feac9f2f0c12000ed87e084b224
alvindivina08 <alvindivina08@gmail.com>
2021-12-20 01:35:27 -0600

    leet code RomanToInteger
    added alot of printouts, so I can understand what is going on in the algorithm. we just basically subtract the previous letter twice if it's greater than what we're currently checking to get a new result.

--- src/main/java/LeetCode/RomanToInteger.java
+++ src/main/java/LeetCode/RomanToInteger.java
@@ -21,28 +21,43 @@ public class RomanToInteger {
         map.put('M', 1000);
 
         int result = 0;
+
         for (int i = 0; i < s.length(); i++) {
+
+
             System.out.println("====================================");
             System.out.println("LOOP: " + i);
             System.out.println("String: " + s.charAt(i));
             System.out.println("map.get: " + map.get(s.charAt(i)));
-            if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
-                System.out.println("if " + i + " is greater than 0 and " + map.get(s.charAt(i)) + " is greater than map.get(s.charAt(i - 1)) which is " + map.get(s.charAt(i - 1)));
 
-                System.out.println("will be calculated as result "+ result + " plus " + map.get(s.charAt(i)) + " - 2 * " + map.get(s.charAt(i - 1)));
+
+            if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
+
+
+                System.out.println("if " + i + " is greater than 0 and " + map.get(s.charAt(i)) + " is greater than map.get(s.charAt(i - 1)) which is " + map.get(s.charAt(i - 1)));
+                System.out.println("will be calculated as result " + result + " plus " + map.get(s.charAt(i)) + " - 2 * " + map.get(s.charAt(i - 1)));
                 System.out.println("result is " + result + ", map.get(s.charAt(" + i + ")) is " + map.get(s.charAt(i)) + ", map.get(s.charAt(i - 1) is " + map.get(s.charAt(i - 1)) + ", 2 * map.get(s.charAt(i - 1) is " + 2 * map.get(s.charAt(i - 1)));
+
+
                 result += map.get(s.charAt(i)) - 2 * map.get(s.charAt(i - 1));
+
+
                 System.out.println("after calculation, result is now " + result);
-                System.out.println("after calculation, "+ "s.charAt(i) is " + s.charAt(i) +" and result is now " + result);
+                System.out.println("after calculation, " + "s.charAt(i) is " + s.charAt(i) + " and result is now " + result);
                 System.out.println("====================================");
 
             } else {
+
                 System.out.println("else");
                 System.out.println("result is currently " + result + ", to be calculated as " + result + " plus " + map.get(s.charAt(i)));
                 System.out.println("result: " + result + " + map.get: " + map.get(s.charAt(i)));
+
+
                 result += map.get(s.charAt(i));
+
+
                 System.out.println("equals " + result);
-                System.out.println("after calculation, "+ "s.charAt(i) is " + s.charAt(i) +" and result is now " + result);
+                System.out.println("after calculation, " + "s.charAt(i) is " + s.charAt(i) + " and result is now " + result);
                 System.out.println("====================================");
             }
         }
@@ -61,7 +76,7 @@ public class RomanToInteger {
 
         int result = map.get(s.charAt(s.length() - 1));
         for (int i = s.length() - 2; i >= 0; i--) {
-            if (map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
                 result -= map.get(s.charAt(i));
             } else {
                 result += map.get(s.charAt(i));
