52e1d4c8b69c12ac392fa32e67706426cd8a8594
alvindivina08 <alvindivina08@gmail.com>
2021-12-28 01:56:24 -0600

    ValidParentheses
    solution

--- src/main/java/LeetCode/ValidParentheses.java
+++ src/main/java/LeetCode/ValidParentheses.java
@@ -1,11 +1,25 @@
 package LeetCode;
 
+import java.util.Stack;
+
 public class ValidParentheses {
     public boolean isValid(String s) {
-        if (s.contains("()") || s.contains("{}") || s.contains("[]")) {
-            return true;
-        } else {
-            return false;
+        if (s.length()%2 !=0) return false;
+
+        Stack<Character> stack = new Stack();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[' ) {
+                stack.push(c);
+            } else if ( c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if ( c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if ( c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
         }
+        return stack.isEmpty();
     }
 }
