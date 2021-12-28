7170f78ee1868ca2863b11a4039c36be98c28d56
alvindivina08 <alvindivina08@gmail.com>
2021-12-28 00:11:21 -0600

    LongestCommonPrefix
    prefix solution

--- src/main/java/LeetCode/LongestCommonPrefix.java
+++ src/main/java/LeetCode/LongestCommonPrefix.java
@@ -5,7 +5,9 @@ public class LongestCommonPrefix {
         if (strs.length == 0 ) return "";
         String prefix = strs[0];
         for (int i = 1 ; i < strs.length ; i++ ) {
+            System.out.println(strs[i]);
             while (strs[i].indexOf(prefix) != 0 ) {
+                System.out.println(prefix);
                 prefix = prefix.substring(0,prefix.length() - 1);
                 if (prefix.isEmpty()) return "";
             }
